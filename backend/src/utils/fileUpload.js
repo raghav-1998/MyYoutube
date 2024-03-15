4dd186049ab1f7cdf71518aed9c19e963d032ae7
raghav-1998 <raghav.vasdeva@gmail.com>
2024-03-15 16:48:17 +0530

    Created Util for File Uploading on Cloudinary from Server

---
+++ backend/src/utils/fileUpload.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary= async (localpath)=>{
+    try{
+        if(!localpath){
+            return null
+        }
+        const response= await cloudinary.uploader.upload(localpath,{
+            resource_type:"auto"
+        })
+
+        fs.unlinkSync(localpath);
+        return response;;
+    }
+    catch(error){
+        fs.unlinkSync(localpath)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
