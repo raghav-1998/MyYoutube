c2d384c9eb85fc8c0c770065c9fcf069d6c92794
raghav-1998 <raghav.vasdeva@gmail.com>
2024-03-20 12:35:50 +0530

    Fixing minor bugs

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -56,11 +56,11 @@ userSchema.pre("save", async function (next){
 })
 
 //Custom method for comparing password
-userSchema.method.isPasswordCorrect=async function(password){
+userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.method.generateAccessToken=function(){
+userSchema.methods.generateAccessToken=function(){
     return jwt.sign({
         _id:this._id,
         email:this.email,
@@ -71,7 +71,7 @@ userSchema.method.generateAccessToken=function(){
     })
 }
 
-userSchema.method.generateRefreshToken=function(){
+userSchema.methods.generateRefreshToken=function(){
     return jwt.sign({
         _id:this._id,
     },process.env.REFRESH_TOKEN_SECRET,{
